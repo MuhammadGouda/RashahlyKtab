dae1d8aced9ae578e870e3e64d633e764aba84b7
Hesham A. Amin <hspceng@gmail.com>
2014-11-01 12:56:01 +0000

    merging DB contexts

--- trunk/RashahlyKtab/Models/IdentityModels.cs
+++ trunk/RashahlyKtab/Models/IdentityModels.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace RashahlyKtab.Models
 {
@@ -19,6 +20,10 @@ namespace RashahlyKtab.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Event> Events { get; set; }
+
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
