2067c3229e5764940b200e1be779582aae65b531
Hesham A. Amin <hspceng@gmail.com>
2014-11-08 18:14:04 +0000

    Add ConfigReader - fix FB share pluging URL

---
+++ trunk/RashahlyKtab/Utilities/ConfigReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace RashahlyKtab.Utilities
+{
+    public static class ConfigReader
+    {
+        public static string DomainName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["DomainName"];
+            }
+        }
+
+    }
+}
