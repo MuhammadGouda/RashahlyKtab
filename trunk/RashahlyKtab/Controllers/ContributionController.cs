c6e9d4e7403413173ed44d83d4845b6616107794
ahmed.hosam <ahmed.hosameldein@gmail.com>
2014-11-09 22:29:49 +0200

    Clean ContributionController , Add updateStatus-controller

--- trunk/RashahlyKtab/Controllers/ContributionController.cs
+++ trunk/RashahlyKtab/Controllers/ContributionController.cs
@@ -17,91 +17,6 @@ namespace RashahlyKtab.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET api/Contribution
-        public async Task<List<Contribution>> GetContributions()
-        {
-            return await db.Contributions.ToListAsync();
-        }
-
-        // GET api/Contribution/5
-        [ResponseType(typeof(Contribution))]
-        public async Task<IHttpActionResult> GetContribution(int id)
-        {
-            Contribution contribution = await db.Contributions.FindAsync(id);
-            if (contribution == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(contribution);
-        }
-
-        // PUT api/Contribution/5
-        public async Task<IHttpActionResult> PutContribution(int id, Contribution contribution)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != contribution.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(contribution).State = EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ContributionExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST api/Contribution
-        [Authorize()]
-        [ResponseType(typeof(Contribution))]
-        public async Task<IHttpActionResult> PostContribution(Contribution contribution)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Contributions.Add(contribution);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = contribution.Id }, contribution);
-        }
-
-        // DELETE api/Contribution/5
-        [ResponseType(typeof(Contribution))]
-        public async Task<IHttpActionResult> DeleteContribution(int id)
-        {
-            Contribution contribution = await db.Contributions.FindAsync(id);
-            if (contribution == null)
-            {
-                return NotFound();
-            }
-
-            db.Contributions.Remove(contribution);
-            await db.SaveChangesAsync();
-
-            return Ok(contribution);
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -111,9 +26,6 @@ namespace RashahlyKtab.Controllers
             base.Dispose(disposing);
         }
 
-        private bool ContributionExists(int id)
-        {
-            return db.Contributions.Count(e => e.Id == id) > 0;
-        }
+        
     }
 }
